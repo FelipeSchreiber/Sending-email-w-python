c9b24a72aa93286a5c0d50dc166cd21e762bab7e
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2019-07-30 20:24:10 -0300

    Add files via upload

--- sendEmail.py
+++ sendEmail.py
@@ -2,9 +2,11 @@ import smtplib
 from getpass import getpass
 from simplecrypt import encrypt, decrypt
 from base64 import b64encode, b64decode
+from email.mime.text import MIMEText
 
 def sendEmail(filename,to,subject,body):
 	gmail_user = 'schreiber.felipe@gmail.com'
+	to = ", ".join(to)
 	encoded_cipher = bytearray()
 	with open(filename, 'rb') as f:
 		encoded_cipher = bytearray(f.read())
@@ -13,20 +15,13 @@ def sendEmail(filename,to,subject,body):
 	password = getpass()
 	gmail_password = str(decrypt(password, cipher))
 	gmail_password = gmail_password[2:-1]
-	sent_from = gmail_user
-	#to = ['schreiber.felipe@gmail.com','schreiber.bruna@gmail.com']
-	#subject = 'OMG Super Important Message'
-	#body = 'Hey, what\'s up?\n\n- You'
-
-	email_text = """\
-	From: %s
-	To: %s
-	Subject: %s
-
-	%s
-	""" % (sent_from, ", ".join(to), subject, body)
-
+	msg = MIMEText(body)
+	msg['Subject'] = subject
+	msg['From'] = gmail_user
+	msg['To'] = to
+	#print(msg.as_string())
 	server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	server.ehlo()
 	server.login(gmail_user, gmail_password)
-	server.sendmail(gmail_user,to,email_text)
+	server.sendmail(gmail_user,to,msg.as_string())
 	server.quit()
